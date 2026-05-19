8999a4fa5b60f914fce1d0853d4b8f58f2478fed
kohee <kiaff450@gmail.com>
2026-05-19 08:57:04 +0000

    main

--- book.s
+++ book.s
@@ -9,3 +9,23 @@
 8 -> easy to learn 
 9 -> easy to learn 
 10 -> easy to learn
+______________________________________________________________________
+before learning riscv asm i  must learn this 
+1 -> a complete knowledge of how a cpu does works 
+2 -> what is a control unit 
+3 -> what is alu 
+4 -> what is signed and unsigned .....(advance vew) 
+5 -> what is ones compliment ?
+6 -> what is 2's compliment ? 
+7 -> basics of floating point 
+8 -> all workflow of ram , cache , stack and heap 
+9 -> all about general purpose registers , what are they 
+10 -> what is programm counter or pc ?
+11 -> what is stack pointer ? why ?
+12 -> what is global pointer ? why ?
+13 -> what is thread pointer ? why ?
+14 -> what is hardwared zero ? and why we need this 
+15 -> basics of abi
+16 -> all basics shift operations 
+17 -> deeper vew of lsb and msb 
+ -> how to diff  between memory and registers in real life 

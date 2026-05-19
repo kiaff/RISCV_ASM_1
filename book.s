a568e5311de3f2c0cf73a68f98200432d853921a
kohee <kiaff450@gmail.com>
2026-05-19 07:02:00 +0000

    main

--- book.s
+++ book.s
@@ -1,1 +1,11 @@
-
+10 reason to learn riscv asm 
+1 -> easy to learn 
+2 -> easy to learn
+3 -> easy to learn 
+4 -> easy to learn 
+5 -> easy to learn 
+6 -> easy to learn 
+7 -> easy to learn 
+8 -> easy to learn 
+9 -> easy to learn 
+10 -> easy to learn
